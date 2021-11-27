1d363865dfd383b899bbb8ac7263a580138da80f
eslam <eslam.hamdi.hassan@gmail.com>
2021-11-27 17:51:08 +0200

    commit 2: searching for devices and connecting to available device finished

--- app/src/main/java/com/eslam/wifip2pchat/ChatBroadCastReciever.java
+++ app/src/main/java/com/eslam/wifip2pchat/ChatBroadCastReciever.java
@@ -1,8 +1,10 @@
 package com.eslam.wifip2pchat;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.NetworkInfo;
 import android.net.wifi.p2p.WifiP2pManager;
 
 public class ChatBroadCastReciever extends BroadcastReceiver {
@@ -22,27 +24,42 @@ public class ChatBroadCastReciever extends BroadcastReceiver {
         this.activity = activity;
 
     }
+    @SuppressLint("MissingPermission")
     @Override
     public void onReceive(Context context, Intent intent) {
         String action = intent.getAction();
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
             // Determine if Wifi P2P mode is enabled or not, alert
             // the Activity.
-            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
-            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                activity.setIsWifiP2pEnabled(true);
-            } else {
-                activity.setIsWifiP2pEnabled(false);
-            }
+//            int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+//            if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+//                activity.setIsWifiP2pEnabled(true);
+//            } else {
+//                activity.setIsWifiP2pEnabled(false);
+//            }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
 
-            // The peer list has changed! We should probably do something about
-            // that.
+
+            if(manager != null)
+            {
+                manager.requestPeers(channel,activity.peerListListener);
+            }
 
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
 
-            // Connection state changed! We should probably do something about
-            // that.
+            if(manager != null) {
+                NetworkInfo networkInfo = (NetworkInfo) intent
+                        .getParcelableExtra(WifiP2pManager.EXTRA_NETWORK_INFO);
+
+                if (networkInfo.isConnected()) {
+
+                    // We are connected with the other device, request connection
+                    // info to find group owner IP
+
+                    manager.requestConnectionInfo(channel, activity.connectionInfoListener);
+
+                }
+            }
 
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
 
--- app/src/main/java/com/eslam/wifip2pchat/P2PChatActivity.java
+++ app/src/main/java/com/eslam/wifip2pchat/P2PChatActivity.java
@@ -1,24 +1,52 @@
 package com.eslam.wifip2pchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.wifi.WpsInfo;
+import android.net.wifi.p2p.WifiP2pConfig;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
+import android.net.wifi.p2p.WifiP2pInfo;
 import android.net.wifi.p2p.WifiP2pManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
 
 import com.eslam.wifip2pchat.databinding.ActivityP2pChatBinding;
 
-public class P2PChatActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import pub.devrel.easypermissions.AppSettingsDialog;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class P2PChatActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
   private   WifiP2pManager manager;
    private WifiP2pManager.Channel channel;
    private BroadcastReceiver receiver;
     private IntentFilter intentFilter;
     private ActivityP2pChatBinding binding;
+    private List<WifiP2pDevice> peers = new ArrayList<WifiP2pDevice>();
+    private String[] deviceNames;
+    ArrayAdapter<String> arrayAdapter;
+    private final Integer REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE = 33;
+    private final Integer REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34;
+
 
 
 
@@ -34,14 +62,77 @@ public class P2PChatActivity extends AppCompatActivity {
         channel = manager.initialize(this, getMainLooper(), null);
         receiver = new ChatBroadCastReciever(manager, channel, this);
         createIntentFilter();
+        arrayAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, new String[]{"empty"});
+        binding.devicesList.setAdapter(arrayAdapter);
 
        turnOn_OffWifi();
+       discoverPeers();
+       listItemsListener();
 
+    }
+
+    private void listItemsListener() {
+
+        binding.devicesList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                final WifiP2pDevice device = peers.get(position);
+                WifiP2pConfig config = new WifiP2pConfig();
+                config.deviceAddress = device.deviceAddress;
+                config.wps.setup = WpsInfo.PBC;
+
+                manager.connect(channel, config, new WifiP2pManager.ActionListener() {
+
+                    @Override
+                    public void onSuccess() {
+                        binding.status.setText("Connected");
+                    }
+
+                    @Override
+                    public void onFailure(int reason) {
+                        Toast.makeText(P2PChatActivity.this, "Connect failed. Retry.",
+                                Toast.LENGTH_SHORT).show();
+                        binding.status.setText("Not Connected");
+
+                    }
+                });
+
+
+
+            }
+        });
+    }
+
+    @SuppressLint("MissingPermission")
+    private void discoverPeers() {
+        binding.discover.setOnClickListener(listener -> {
+            if(hasLocationPermissions(this.getApplicationContext())){
+                manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+                    @Override
+                    public void onSuccess() {
+                        Log.e(null, "onSuccess: succeded");
+                        binding.status.setText("Searching For Nearby Devices");
+                    }
+
+                    @Override
+                    public void onFailure(int i) {
+                        binding.status.setText("Searching Failed!!");
+                    }
+                });
+            }else
+            {
+                requestPermissions();
+            }
+
+        });
     }
 
     private void turnOn_OffWifi() {
         binding.onOff.setOnClickListener(listener -> {
 
+            Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+            startActivityForResult(intent,1);
         })
 
         ;
@@ -73,4 +164,148 @@ public class P2PChatActivity extends AppCompatActivity {
 
     public void setIsWifiP2pEnabled(boolean b) {
     }
+
+
+    WifiP2pManager.PeerListListener  peerListListener = new WifiP2pManager.PeerListListener() {
+            @Override
+            public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
+                if(!wifiP2pDeviceList.getDeviceList().equals(peers))
+                {
+                      peers.clear();
+                      peers.addAll(wifiP2pDeviceList.getDeviceList());
+
+                      deviceNames = new String[wifiP2pDeviceList.getDeviceList().size()];
+
+                      for(int x = 0; x < deviceNames.length; x++)
+                      {
+                          deviceNames[x] = peers.get(x).deviceName;
+                      }
+
+                   arrayAdapter.addAll(deviceNames);
+                      arrayAdapter.notifyDataSetChanged();
+
+                }
+                if(peers.size() == 0)
+                {
+                    binding.status.setText("No Devices Found!!");
+
+                }
+
+            }
+        };
+
+    WifiP2pManager.ConnectionInfoListener connectionInfoListener = new WifiP2pManager.ConnectionInfoListener() {
+        @Override
+        public void onConnectionInfoAvailable(WifiP2pInfo wifiP2pInfo) {
+            String groupOwnerAddress =  wifiP2pInfo.groupOwnerAddress.getHostAddress();
+            ;
+
+            // After the group negotiation, we can determine the group owner
+            // (server).
+            if (wifiP2pInfo.groupFormed && wifiP2pInfo.isGroupOwner) {
+                binding.status.setText("Host");
+            } else if (wifiP2pInfo.groupFormed) {
+                // The other device acts as the peer (client). In this case,
+                // you'll want to create a peer thread that connects
+                // to the group owner.
+                binding.status.setText("Client");
+            }
+
+        }
+    };
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    Boolean hasLocationPermissions(Context context) {
+
+        boolean permissionState;
+
+
+       if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+             permissionState = EasyPermissions.hasPermissions(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            );
+
+        } else {
+            permissionState = EasyPermissions.hasPermissions(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            );
+        }
+
+       return permissionState;
+    }
+
+    void requestPermissions()
+    {
+        if (hasLocationPermissions(this.getApplicationContext()))
+        {
+            return;
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q)
+        {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permissions to use this feature.",
+                    REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            );
+        } else
+        {
+            EasyPermissions.requestPermissions(
+                    this,
+                    "You need to accept location permissions to use this feature.",
+                    REQUEST_FOREGROUND_AND_BACKGROUND_PERMISSION_RESULT_CODE,
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+            );
+        }
+    }
+
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onPermissionsGranted(int requestCode, @NonNull List<String> perms) {
+        initiateActionListener();
+    }
+
+    @SuppressLint("MissingPermission")
+    private void initiateActionListener() {
+        manager.discoverPeers(channel, new WifiP2pManager.ActionListener() {
+            @Override
+            public void onSuccess() {
+                Log.e(null, "onSuccess: succeded");
+                binding.status.setText("Searching For Nearby Devices");
+            }
+
+            @Override
+            public void onFailure(int i) {
+                binding.status.setText("Searching Failed!!");
+                Log.e(null, "onFailure:failed " );
+            }
+        });
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, @NonNull List<String> perms) {
+
+        if (EasyPermissions.somePermissionPermanentlyDenied(this, Collections.singletonList(perms.toString())))
+        {
+            new AppSettingsDialog.Builder(this).build().show();
+        } else
+        {
+            requestPermissions();
+        }
+
+    }
+
+
 }
+
+
